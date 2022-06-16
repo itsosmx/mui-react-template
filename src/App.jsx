f52bae701c3835ca6803e964910d2cddfbcff1b0
itsosmx <o.h1277@yahoo.com>
2022-06-16 23:23:36 +0200

    main

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+export default function App() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>{/* <Route></Route> */}</Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
